244e823706b12be570d64744225a5dd93585cc1a
Fabio Meireles <fbomrl14@gmail.com>
2021-07-14 19:57:32 -0300

    atualização de exercicio método

--- src/curso_programacao/MethodDetectordePesado.java
+++ src/curso_programacao/MethodDetectordePesado.java
@@ -7,29 +7,36 @@ public class MethodDetectordePesado {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int i;
-		String nome="", pesado="";
-		double peso, maior=0.00;
-		
+		String nome = "", pesado = "";
+		double peso, maior = 0;
 
-				
-		System.out.println("----------------------------------");
-		System.out.println("       DETECTOR DE PESADO         ");
-		System.out.println("Maior peso até agora: "+maior+" Kg");
-		System.out.println("----------------------------------");
-		
+		topo(maior);
+
 		for (i = 1; i <= 5; i++) {
 			System.out.println("Digite o nome: ");
 			nome = sc.next();
-			System.out.println("Digite o peso de "+nome+": ");
+			System.out.println("Digite o peso de " + nome + ": ");
 			peso = sc.nextDouble();
-			if (peso >= maior ) {
-					maior = peso;
-					pesado = nome;		
+			if (peso >= maior) {
+				maior = peso;
+				pesado = nome;
 			}
+			topo(maior);	
 		}
-		System.out.println("A pessoa mais pesada foi "+pesado+", com "+maior+" Kg.");
+		topo(maior);
+		System.out.println("A pessoa mais pesada foi " + pesado + ", com " + maior + " Kg.");
 		sc.close();
-		
+
+	}
+
+	private static void topo(double maior) {
+
+		System.out.println("----------------------------------");
+		System.out.println("       DETECTOR DE PESADO         ");
+		System.out.println("Maior peso até agora: " + maior + " Kg");
+		System.out.println("----------------------------------");
+
+
 	}
 
 }
