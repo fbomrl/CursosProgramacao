67fb990a583ece09afb2e63b23dbaa2ad0bd91c3
Fabio Meireles <fbomrl14@gmail.com>
2021-09-01 18:51:06 -0300

    inclue atividades.

--- IntroducaoOrientacaoaObjetosByteBank/src/Conta.java
+++ IntroducaoOrientacaoaObjetosByteBank/src/Conta.java
@@ -3,8 +3,28 @@ public class Conta {
 	int agencia;
 	int numero;
 	String titular;
-	
+
 	public void deposita(double valor) {
-		saldo = saldo + valor;
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if(this.saldo >=valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino){
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+			return false;
+
 	}
 }
--- IntroducaoOrientacaoaObjetosByteBank/src/TestaMetodo.java
+++ IntroducaoOrientacaoaObjetosByteBank/src/TestaMetodo.java
@@ -5,8 +5,27 @@ public class TestaMetodo {
 		Conta conta = new Conta();
 		conta.saldo = 100;
 		conta.deposita(50);
-		System.out.println(conta);
-
+		System.out.println(conta.saldo);
+		
+		boolean	conseguiuretirar = conta.saca(20);
+		System.out.println(conta.saldo);
+		System.out.println(conseguiuretirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		if(contaDaMarcela.transfere(300, conta)) {
+			System.out.println("Transferencia realizada com sucesso! ");
+		}
+		else {
+			System.out.println("Faltou dinheito! ");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(conta.saldo);
+		
+		conta.titular = "Fabio Meireles";
+		System.out.println(conta.titular);
+		
 	}
 
 }
