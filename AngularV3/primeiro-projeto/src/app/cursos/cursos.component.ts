c8458636fcc65934352701e2817b6c09f3febd82
Fábio Meireles <fbomrl14@gmail.com>
2022-09-29 12:21:38 -0300

    Inclui atividades angular

---
+++ AngularV3/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training';
+
+    for (let i=0; i<this.cursos.length; i++){
+      let curso = this.cursos[i];
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+
+}
